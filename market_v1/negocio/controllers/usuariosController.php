5c3b42f21fa38f8cff998b8bb6f804bfdb0e5802
ricardo-munoz_cincel_digital <ricardo@cincel.digital>
2022-10-23 12:27:08 -0300

    chore: refactor model & controller

--- market_v1/negocio/controllers/usuariosController.php
+++ market_v1/negocio/controllers/usuariosController.php
@@ -7,9 +7,48 @@ class UsuariosController
     public function getAll()
     {
         $usuariosModel = new usuariosModel();
-        $rows = $usuariosModel->getAll();
+        $getAll = $usuariosModel->getAll();
         // esto lo puedes hacer en una sola línea
         // $rows = (new usuariosModel())->getAll();
+
+        foreach ($getAll as $key => $result) {
+
+            switch ($result->privilegio) {
+                case 1:
+                    $result->privilegio = 'Administrador';
+                    break;
+                case 2:
+                    $result->privilegio =  'Estandar';
+                    break;
+                case 3:
+                    $result->privilegio =  'Cliente';
+                    break;
+                case 4:
+                    $result->privilegio =  'Empleado';
+                    break;
+                case 5:
+                    $result->privilegio =  'Proveedor';
+                    break;
+                case 6:
+                    $result->privilegio =  'Limitado';
+                    break;
+                default:
+                    $result->privilegio =  '';
+                    break;
+            }
+
+            $result->nacimiento =  (new DateTime($result->nacimiento))->format("d-m-Y");
+
+            $result->botones = "";
+            if (5 === (int)$result->privilegio) {
+                $result->botones .= "<a class=\"btn btn-primary\" href=\"vercuenta.php?idusuario={$result->idusuario}\">Ver cuenta</a>";
+            }
+
+            $result->botones .= "<a class=\"btn btn-success\" href=\"formularioUpdate.php?idusuario={$result->idusuario}\">Modificar</a>";
+            $result->botones .= "<a class=\"btn btn-danger\" onclick=\"return confirm ('&iquest;Desea eliminar el registro seleccionado?');\" href=\"formularioEliminar.php?idusuario={$result->idusuario}\"> Eliminar</a>";
+
+            $rows[] = $result;
+        }
 
         if ($rows) {
             include_once('../views/usuarios/usuariosViewGetAll.php');
--- market_v1/negocio/models/usuariosModel.php
+++ market_v1/negocio/models/usuariosModel.php
@@ -10,43 +10,8 @@ class UsuariosModel extends conexion
     {
         $this->consulta = $this->con->query("SELECT * FROM usuarios");
         while ($result = $this->consulta->fetch_object()) {
-            // var_dump($result);
-            switch ($result->privilegio) {
-                case 1:
-                    $result->privilegio = 'Administrador';
-                    break;
-                case 2:
-                    $result->privilegio =  'Estandar';
-                    break;
-                case 3:
-                    $result->privilegio =  'Cliente';
-                    break;
-                case 4:
-                    $result->privilegio =  'Empleado';
-                    break;
-                case 5:
-                    $result->privilegio =  'Proveedor';
-                    break;
-                case 6:
-                    $result->privilegio =  'Limitado';
-                    break;
-                default:
-                    $result->privilegio =  '';
-                    break;
-            }
-            $result->nacimiento =  (new DateTime($result->nacimiento))->format("d-m-Y");
-
-            $result->botones = "";
-            if (5 === (int)$result->privilegio) {
-                $result->botones .= "<a class=\"btn btn-primary\" href=\"vercuenta.php?idusuario={$result->idusuario}\">Ver cuenta</a>";
-            }
-
-            $result->botones .= "<a class=\"btn btn-success\" href=\"formularioUpdate.php?idusuario={$result->idusuario}\">Modificar</a>";
-            $result->botones .= "<a class=\"btn btn-danger\" onclick=\"return confirm ('&iquest;Desea eliminar el registro seleccionado?');\" href=\"formularioEliminar.php?idusuario={$result->idusuario}\"> Eliminar</a>";
-
             $this->rows[] = $result;
         }
-        // var_dump($this->rows);
         return $this->rows;
     }
 }
